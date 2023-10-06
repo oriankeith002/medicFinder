6f7d6d72cd5f3d1770b1544249bb521a2dab0d69
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-06 16:58:03 +0300

    Added rating model

---
+++ medicSearch/msearch/models/Ratings.py
@@ -0,0 +1,14 @@
+from msearch.models import *
+
+class Rating(models.Model):
+
+    user = models.ForeignKey(User, related_name='rator', on_delete=models.CASCADE);
+    user_rated = models.ForeignKey(User, related_name='rated', on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    opinion = models.TextField(null=True, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return 'Evaluator: {} | Rated: {}'.format(self.user.first_name, self.user_rated)
