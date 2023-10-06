4f3190adc42906ad38affd363c8e9d33bbeca721
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-06 16:11:43 +0300

    Setting up Profiles Table via Profile model

--- medicSearch/medicSearch/settings.py
+++ medicSearch/medicSearch/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # installed apps
-    msearch,
+    'msearch.apps.MsearchConfig',
 ]
 
 MIDDLEWARE = [
--- medicSearch/msearch/models/Profile.py
+++ medicSearch/msearch/models/Profile.py
@@ -1,30 +1,33 @@
 
-# from msearch.models import *
+from msearch.models import *
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     role = models.IntegerChoices(choices=ROLE_CHOICE, default=3)
-#     birthday = models.DateField()
-#     created_at = models.DateTimeField()
-#     updated_at = models.DateTimeField()
-#     token = models.CharField()
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.IntegerChoices(choices=ROLE_CHOICE, default=3)
+    birthday = models.DateField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    token = models.CharField()
 
-#     def __str__(self):
-#         return '{}'.format(self.user.username)  
+    def __str__(self):
+        return '{}'.format(self.user.username)  
     
 
-#     @receiver(post_save,sender=User)
-#     def create_user_profile(sender, instance, created,**kwargs):
-#         try:
-#             if created:
-#                 Profile.object.create(user=instance)
-#         except: 
-#             pass 
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender, instance, created,**kwargs):
+        '''
+        Info: Method to create the user profile
+        '''
+        try:
+            if created:
+                Profile.object.create(user=instance)
+        except: 
+            pass 
 
 
-#     @receiver(post_save, sender=User)
-#     def save_user_pofile(sender, instance, **kwargs):
-#         try: 
-#             instance.profile.save()
-#         except:
-#             pass
+    @receiver(post_save, sender=User)
+    def save_user_pofile(sender, instance, **kwargs):
+        try: 
+            instance.profile.save()
+        except:
+            pass
