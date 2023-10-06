dc6f5d7daf21b3f1d537132f759bb311e4fc10b5
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-06 16:43:59 +0300

    Added Doctor's address Model

--- medicSearch/msearch/models/Address.py
+++ medicSearch/msearch/models/Address.py
@@ -2,7 +2,7 @@ from msearch.models import *
 
 class Address(models.Model):
 
-    neighborhood = models.ForeignKey()
+    neighborhood = models.ForeignKey(Neighborhood, null=True, related_name="neighborhood", on_delete=models.SET_NULL)
     name = models.CharField(null=False, max_length=100)
     address = models.CharField(null=False, max_length=255)
     latitude = models.DecimalField(max_digits=9, decimal_places=7)
