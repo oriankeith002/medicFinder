89bce03144e462973bd52bce88f1ddd01e96d532
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-10-06 15:36:36 +0300

    Initial project setup and setting user profiles

---
+++ medicSearch/msearch/models/Profile.py
@@ -0,0 +1,30 @@
+
+# from msearch.models import *
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     role = models.IntegerChoices(choices=ROLE_CHOICE, default=3)
+#     birthday = models.DateField()
+#     created_at = models.DateTimeField()
+#     updated_at = models.DateTimeField()
+#     token = models.CharField()
+
+#     def __str__(self):
+#         return '{}'.format(self.user.username)  
+    
+
+#     @receiver(post_save,sender=User)
+#     def create_user_profile(sender, instance, created,**kwargs):
+#         try:
+#             if created:
+#                 Profile.object.create(user=instance)
+#         except: 
+#             pass 
+
+
+#     @receiver(post_save, sender=User)
+#     def save_user_pofile(sender, instance, **kwargs):
+#         try: 
+#             instance.profile.save()
+#         except:
+#             pass
---
+++ medicSearch/msearch/models/__init__.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
+
+
+ROLE_CHOICE = (
+    (1,'Admin'),
+    (2, 'Medical Staff'),
+    (3, 'Patient'),
+)
+
+from .Profile import Profile
